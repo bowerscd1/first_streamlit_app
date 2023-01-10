7a8013c1cc08a7b77dfd572e8093d171858cb405
bowerscd1 <121956277+bowerscd1@users.noreply.github.com>
2023-01-10 14:12:37 -0600

    Update streamlit_app.py
    Add Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry')
-streamlit.text('Kale, Spinach, Roket Smothie ')
-streamlit.text('Hard-Boiled Egg')
+streamlit.header('🍽Breakfast Menu🍴')
+streamlit.text('🥣Omega 3 & Blueberry')
+streamlit.text('🍃Kale, Spinach, Roket Smothie ')
+streamlit.text('🥚Hard-Boiled Egg')
